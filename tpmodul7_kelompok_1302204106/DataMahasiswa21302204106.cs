d1a8cf0f197cad27ec1b1132ddaea06cff10856c
faraslutfi <127936502+faraslutfi@users.noreply.github.com>
2025-04-06 22:08:51 +0700

    Menambahkan class DataMahasiswa21302204106 dan implementasi deserialisasi JSON untuk data mata kuliah.

---
+++ tpmodul7_kelompok_1302204106/DataMahasiswa21302204106.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace tpmodul7_kelompok_1302204106
+{
+    public class DataMahasiswa21302204106
+    {
+        public class MataKuliah
+        {
+            public string? code { get; set; }
+            public string? name { get; set; }
+        }
+
+        public class CourseList
+        {
+            public List<MataKuliah>? courses { get; set; }
+        }
+
+        public static void ReadJSON2()
+        {
+            string json = File.ReadAllText("tp7_2_1302204106.json");
+            CourseList? list = JsonConvert.DeserializeObject<CourseList>(json);
+
+            Console.WriteLine("Daftar Mata Kuliah:");
+            if (list?.courses != null)
+            {
+                foreach (var course in list.courses)
+                {
+                    Console.WriteLine($"{course.code} - {course.name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tidak ada data matkul.");
+            }
+        }
+    }
+}
