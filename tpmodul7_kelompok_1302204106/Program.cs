fe7669da225d54229a172f6edcb13d32e14f3505
faraslutfi <127936502+faraslutfi@users.noreply.github.com>
2025-04-06 21:43:30 +0700

    menambahkan class DataMahasiswa1302204106

--- tpmodul7_kelompok_1302204106/Program.cs
+++ tpmodul7_kelompok_1302204106/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace tpmodul7_kelompok_1302204106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Panggil method ReadJSON dari class DataMahasiswa1302204106
+            DataMahasiswa1302204106.ReadJSON();
+
+            Console.ReadLine(); // supaya console ga ketutup
+        }
+    }
+}
